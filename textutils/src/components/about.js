a76d7bf00390a8e2cbdd08a755d2357113b0bf9b
vishva2069 <vishvapansuriya.2004@gmail.com>
2026-02-02 18:39:42 +0530

    ..

--- textutils/src/components/about.js
+++ textutils/src/components/about.js
@@ -1,10 +1,41 @@
 import React from 'react'
 
-export default function about() {
+export default function about(useState) {
+
+ 
+   const [mystyle, setMystyle] = useState(   {
+    color: 'black',
+    backgroundColor: 'white'
+  });
+
+  const  [btntext, setbtntext] = useState(
+ "Enable Dark Mode"
+  );
+
+  const setmypath = () =>{
+  if ( mystyle.color == 'white'){
+    setMystyle({
+        color: 'black',
+    backgroundColor: 'white'
+
+    })
+   setbtntext ("Enable Light Mode")
+  }
+  else{
+
+    setMystyle({
+      color: 'white',
+  backgroundColor: 'black'
+    
+  })
+   setbtntext("enable dark mode")
+   setbtntext("Enable dark mode")
+  }
+  
   return (
-    <div className="container">
+    <div className="container" style={myStyle}>
       <h3 className='my-2'>About Us</h3>
-     <div class="accordion accordion-flush" id="accordionFlushExample">
+     <div class="accordion accordion-flush" id="accordionFlushExample" style={myStyle}>
   <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
@@ -12,7 +43,8 @@ export default function about() {
       </button>
     </h2>
     <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item’s accordion body.</div>
+      <div class="accordion-body" style={myStyle}>Placeholder content for this accordion, which is intended to demonstrate the 
+        <code>.accordion-flush</code> class. This is the first item’s accordion body.</div>
     </div>
   </div>
   <div class="accordion-item">
@@ -22,7 +54,9 @@ export default function about() {
       </button>
     </h2>
     <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item’s accordion body. Let’s imagine this being filled with some actual content.</div>
+      <div class="accordion-body" style={myStyle}>Placeholder content for this accordion, which is intended to demonstrate the 
+        <code>.accordion-flush</code> class. This is the second item’s accordion body.
+         Let’s imagine this being filled with some actual content.</div>
     </div>
   </div>
   <div class="accordion-item">
@@ -32,7 +66,7 @@ export default function about() {
       </button>
     </h2>
     <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the 
+      <div class="accordion-body" style={myStyle}>Placeholder content for this accordion, which is intended to demonstrate the 
         <code>.accordion-flush</code> class. This is the third item’s accordion body. 
         Nothing more exciting happening here in terms of content, but just filling up the space to make it look, 
         at least at first glance, a bit more representative of how this would look in a real-world application.</div>
@@ -40,10 +74,12 @@ export default function about() {
   </div>
 </div>
 <div className=' my-3'>
-  <button type="button" class="btn btn-success">Enable dark mode</button>
+
+  <button type="button" onClick={setmypath} className="btn btn-success">Enable dark mode</button>
+
 </div>
 
 </div>
  
-  )
-}
+  )}}
+
