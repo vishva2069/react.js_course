fb88a1fba7956fe87bfc5e4362ea2a0bd7705e00
vishva2069 <vishvapansuriya.2004@gmail.com>
2026-02-03 17:26:04 +0530

    ghhhhh

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -2,33 +2,23 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
-import Alert from './components/Alert';
-import React,{useState} from 'react';
+
 
  
 function App() {
-   const [alert, setAlert] = useState(null);
-
-   showAlert = (message,type) => {
-      setAlert({
-         msg: message,
-         type : type,
-      })
-   }
-   
+ 
   return(
  <>
  
   <Navbar title="Textutils" aboutext="about us "/>
  {/* /*  <Navbar/>
   <Navbar title="Textutils" /> */ }
-  <Alert Alert="the discription to like a style usings before navigation"></Alert>
 
  <div className="container my-3">
   <TextForm heading="Enter the text to analyze"/>
   </div>
   <About className="container"/>
-  <Alert alert={alert}/>
+ 
  
  </>
 
