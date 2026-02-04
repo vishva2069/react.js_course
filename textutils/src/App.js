da9e3cdd2d263af6ab04e44bbaabfacaf355509e
vishva2069 <vishvapansuriya.2004@gmail.com>
2026-02-04 17:16:23 +0530

    tt

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -9,19 +9,21 @@ import React,{useState} from 'react';
 function App() {
   const [mode, setMode] = useState('light');
 
-
-
-
   const togglemode = () => {
    if (mode === 'light') {
       setMode('dark');
      document.body.style.backgroundColor = '#04273';
-    
-    
+     document.title = 'Textutils - Dark Mode';
+     setInterval(() => {
+      document.title = 'textutils is amazing like this';
+     }, 2500);
+     setInterval(() => {
+      document.title = 'install textutils is amazing like this to style';
+     }, 1500);
    } else {
       setMode('light');
      document.body.style.backgroundColor = 'white';
-     
+     document.title = 'Textutils - Light Mode';
    }
 }
   return(
