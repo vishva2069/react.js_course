d4ecd5ec2c7281c172dc9b3f082f1089fc97b2b7
vishva2069 <vishvapansuriya.2004@gmail.com>
2026-02-09 18:31:37 +0530

    rr

--- textutils/src/index.js
+++ textutils/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter} from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,11 +14,6 @@ root.render(
   
 );
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
-  <App />
-</HashRouter>
-);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
